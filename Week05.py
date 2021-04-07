3a82945e49a9f7554c72022bc3a42235c79b3158
Ciara O Sullivan <o_sullivan_ciara@lilly.com>
2021-04-07 15:48:17 +0100

    Updating

---
+++ Week05.py
@@ -0,0 +1,14 @@
+#Week 5 - $ python collatz.py
+#Author: Ciara O Sullivan 
+
+import datetime
+
+weekday = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+weekend = ["Saturday", "Sunday"]
+
+response =input("What day is it today?")
+
+if response = weekday
+    print ("Yes today is a weekday! Nearly there.")
+else respone == weekend 
+    print ("Yay, it's the weekend!!!!")
