dd04c6efd2c5238bb3cdb4749f564b73e7fe68eb
Ciara O Sullivan <o_sullivan_ciara@lilly.com>
2021-04-10 12:56:55 +0100

    More updates

--- Week08.py
+++ Week08.py
@@ -3,26 +3,32 @@
 #Author: Ciara O Sullivan 
 #Write a program that displays a plot of the functions 
 
-#pip install matplotlib
+
 import matplotlib.pyplot as plt
 import numpy as np 
-#plt.plot ([0, 1, 2, 3, 4])
-#plt.ylabel('some numbers')
-#plt.show()
 
 
-# f(x)=x, g(x)=x2 and h(x)=x3 
+#f(x)=x, g(x)=x2 and h(x)=x3 
 #f =2, g = 4, h = 8
 
-x =2 
+allTimes_x = [2,4,8] #creating values for the x axis
+dev_y =[2, 3, 4] #creating values for the y axis
+sec_y =[3,5,9] #creating second set of values for the y axis so we can compare
 
-def g(x2):
-    return x2
-y =np.linspace(0,4,501)
+plt.plot(allTimes_x, dev_y, color ="m", linestyle="--", marker ="o", linewidth ="3") #asking the system to plot the both, making the line dash-dash and the colour purple
+plt.plot(allTimes_x,sec_y, color ="#0040ff", marker= "s") #plotting the second values also adding square markers to this line, adding a certain hex colour. 
 
-#values =g(x2)
-#plt.plot(y, values, 'r-')
-plt.xlabel('$y$'); plt.ylabel('$g(y)$')
-plt.title('Damped sine wave')
-#plt.savefig('tmp.png'); plt.savefig('tmp.pdf')
-plt.show()
+plt.title("My First Attempt at a Plot") #Naming the plot title
+
+plt.xlabel("X-Label") #naming the x axis label
+plt.ylabel ("Y-Label") #naming the y axis label
+plt.legend(["Yesterday", "Today"]) #naming the legend of the two set of values
+
+plt.grid(True) #creating a grid on the plot
+
+plt.show() #asking for the program to show the plot visually
+
+# plt.savefig("Plot.png") #saving your plotted figure to your computer
+
+
+# ds.plot.scatter(x="A", y="B", hue="w", hue_style="discrete") 
